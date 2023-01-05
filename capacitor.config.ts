8c7b901e5d6c5f5bd4542b45183c35c440883ded
imane.qajjou <qajjouimane@gmail.com>
2023-01-05 09:52:09 +0100

    commit capacitor v1

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'projet-voyages',
+  webDir: 'dist/projet-voyages',
+  bundledWebRuntime: false
+};
+
+export default config;
